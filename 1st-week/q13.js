1a8f1111844c011b128fa9ce625dc68b6d02e416
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-03-19 00:11:40 +0530

    Q13 Completed

---
+++ 1st-week/q13.js
@@ -0,0 +1,9 @@
+// Accept the gender from the user as char and print the respective greeting message(Ex - Good Morning Sir(on the basis of gender))
+
+let gender = prompt("Enter your gender:");
+
+if (gender === "male") {
+    alert(`Good Morning Sir.`);
+} else {
+    alert(`Good Morning Ma'am.`);
+}
