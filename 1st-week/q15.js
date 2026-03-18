da54181c0acb33cbfd6ae44fb3031779e7e9af33
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-03-19 00:12:06 +0530

    Q15 Completed

---
+++ 1st-week/q15.js
@@ -0,0 +1,9 @@
+// Accept an integer and check whether it is an even number or odd.
+
+let num = prompt("Enter a Number to check Even/Odd: ");
+
+if (num % 2 == 0) {
+    alert(num + " is an Even Number.");
+} else {
+    alert(num + " is an Odd Number.");
+}
