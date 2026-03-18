2d76bc2c8d1be6ac88b1a912818f51d0c76c3091
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-03-19 00:10:53 +0530

    Q11 Completed

---
+++ 1st-week/q11.js
@@ -0,0 +1,9 @@
+// Find circumference and area of circle
+
+const r = 10;
+
+const circumference = 2 * Math.PI * r;
+const area = Math.PI * r * r;
+
+console.log(`Circumference = ${circumference}`);
+console.log(`Area = ${area}`);
