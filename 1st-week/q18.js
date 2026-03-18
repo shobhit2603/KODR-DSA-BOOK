cc91d025a74b737f06a4ea87eed8682ed8f125f1
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-03-19 00:12:59 +0530

    Q18 Completed

---
+++ 1st-week/q18.js
@@ -0,0 +1,13 @@
+// Accept three numbers and find the greatest number among them.
+
+let a = prompt("Enter first number: ");
+let b = prompt("Enter second number: ");
+let c = prompt("Enter third number: ");
+
+if (a > b && a > c) {
+    alert(`${a} is the greatest number.`);
+} else if (b > a && b > c) {
+    alert(`${b} is the greatest number.`);
+} else {
+    alert(`${c} is the greatest number.`);
+}
