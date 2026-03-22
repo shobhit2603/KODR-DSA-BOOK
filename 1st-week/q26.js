cb7fbcb15e963e9223fedbac2e2c2a494d2327d2
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-03-22 23:21:12 +0530

    Completed Q27

--- 1st-week/q26.js
+++ 1st-week/q26.js
@@ -1,4 +1,6 @@
-let n = 5;
+// Take a number as input and print its table (Ex: 5 * 1 = 5 ... up to 10 terms)
+
+let n = prompt("Enter a number to print its Table: ");
 
 for (let i = 1; i <= 10; i++) {
     console.log(`${n} * ${i} = ${n * i}`);
---
+++ 1st-week/q27.js
@@ -0,0 +1,10 @@
+// Sum up to n terms.
+
+let n = 10;
+
+let sum = 0;
+
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
+console.log(sum);
