d10a9bed3d0babba5542094df8fdde8437ee1c28
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-03-23 16:09:38 +0530

    Completed 53 Questions

---
+++ 1st-week/q52.js
@@ -0,0 +1,16 @@
+// V Pattern
+
+let n = 5;
+
+for (let i = 0; i < n; i++) {
+    let row = '';
+
+    for (let j = 0; j < 2 * n - 1; j++) {
+        if (j === i || j === (2 * n - 2 - i)) {
+            row += '*';
+        } else {
+            row += ' ';
+        }
+    }
+    console.log(row);
+}
---
+++ 1st-week/q53.js
@@ -0,0 +1,15 @@
+// X Pattern
+
+let n = 10;
+
+for (let i = 1; i <= n; i++) {
+    let str = "";
+    for (let j = 1; j <= n; j++) {
+        if (i === j || i + j === n + 1) {
+            str += "*";
+        } else {
+            str += " ";
+        }
+    }
+    console.log(str);
+}
